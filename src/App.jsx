6ee7f822c45e9fc129c4b9d097e7ce61d8a92b6c
Jeevamk <138573828+Jeevamk@users.noreply.github.com>
2024-04-09 12:01:08 +0530

    phone number

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
-import  { useState } from 'react';
+import { useState } from 'react';
 import axios from 'axios';
-import './App.css';
 
 function App() {
   const [phoneNumber, setPhoneNumber] = useState('');
@@ -9,36 +8,35 @@ function App() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post('https://chimpu.xyz/api/post.php', { phonenumber: phoneNumber });
-
-      let headersData = '';
-      Object.entries(response.headers).forEach(([key, value]) => {
-        headersData += `${key}: ${value}\n`;
-      });
+      const response = await axios.post('https://chimpu.xyz/api/post.php', { Post: phoneNumber });
+      console.log(response.headers);
+      const headersData = JSON.stringify(response.headers);
 
       setResponseData(headersData);
+      
     } catch (error) {
       console.error('Error:', error);
     }
   };
 
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <label>
+    <div className="container mx-auto px-4 py-8">
+      <form onSubmit={handleSubmit} className="max-w-md mx-auto ">
+        <label className="block mb-4">
           Phone Number:
           <input
-            type="text"
-            value={phoneNumber}
+            type="number"
+            value={phoneNumber} 
             onChange={(e) => setPhoneNumber(e.target.value)}
+            className="block w-full mt-1 p-2 border border-gray-300 rounded"
           />
         </label>
-        <button type="submit">Submit</button>
+        <button type="submit" className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600">Submit</button>
       </form>
       {responseData && (
         <div>
-          <h2>Response Headers:</h2>
-          <pre>{responseData}</pre>
+          <h2 className="mt-8 text-lg font-semibold">Response Headers:</h2>
+          <pre className="mt-2 bg-gray-100 p-4">{responseData}</pre>
         </div>
       )}
     </div>
